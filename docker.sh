97bf38f09b1d1882f522cb8aef154bd44b086c06
manaunsd <110738811+manaunsd@users.noreply.github.com>
2022-09-09 00:36:57 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,3 @@
 sudo apt install apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test" && sudo apt update && sudo apt install docker-ce -y
+
+sudo dpkg --add-architecture i386 && sudo apt-get update -y && sudo apt-get install wine32:i386
